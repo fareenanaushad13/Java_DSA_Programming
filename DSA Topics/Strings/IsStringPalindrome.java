2b0567de75f3cdc19f04fc4a95ebf893129267e4
fareenanaushad13 <114231603+fareenanaushad13@users.noreply.github.com>
2025-02-11 00:24:49 +0530

    String

---
+++ DSA Topics/Strings/IsStringPalindrome.java
@@ -0,0 +1,15 @@
+public class IsStringPalindrome{
+    public static boolean isPalindrome(String str){
+        for(int i=0;i<str.length()/2;i++){
+            int n = str.length();
+            if(str.charAt(i)!=str.charAt(n-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String args[]){
+        String str = "farfarru";
+        System.out.println(isPalindrome(str));
+    }
+}
