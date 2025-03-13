34ee9558246e9e0d92a3fe4cc2164082b894ba36
fareenanaushad13 <114231603+fareenanaushad13@users.noreply.github.com>
2025-03-13 21:49:24 +0530

    Tarjan's Bridge Algorithm

--- DSA Topics/Graphs/TarjansBridge.java
+++ DSA Topics/Graphs/TarjansBridge.java
@@ -1,8 +1,9 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class TarjansBridge {
-    static class Edge {
-        int src, dest;
+     static class Edge {
+        int src;
+        int dest;
 
         public Edge(int s, int d) {
             this.src = s;
@@ -10,11 +11,12 @@ public class TarjansBridge {
         }
     }
 
-    static void createGraph(ArrayList<Edge> graph[]) {
+     static void createGraph(ArrayList<Edge> graph[]) {
         for (int i = 0; i < graph.length; i++) {
             graph[i] = new ArrayList<>();
         }
 
+       
         graph[0].add(new Edge(0, 1));
         graph[0].add(new Edge(0, 2));
         graph[0].add(new Edge(0, 3));
@@ -38,16 +40,17 @@ public class TarjansBridge {
         graph[5].add(new Edge(5, 4));
     }
 
-    public static void dfs(ArrayList<Edge>[] graph, int curr, int par, int dt[], int low[], boolean vis[], int time[]) {
+    public static void dfs(ArrayList<Edge>[] graph, int curr, int par, int dt[], int low[], boolean vis[], int time) {
         vis[curr] = true;
-        dt[curr] = low[curr] = time[0]++;
-        
-        for (Edge e : graph[curr]) {
+        dt[curr] = low[curr] = ++time;
+
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
             int neigh = e.dest;
 
-            if (neigh == par) continue; // Ignore parent
-            
-            if (!vis[neigh]) {
+            if (neigh == par) {
+                continue;
+            } else if (!vis[neigh]) {
                 dfs(graph, neigh, curr, dt, low, vis, time);
                 low[curr] = Math.min(low[curr], low[neigh]);
 
@@ -63,16 +66,15 @@ public class TarjansBridge {
     public static void tarjanBridge(ArrayList<Edge>[] graph, int V) {
         int dt[] = new int[V];
         int low[] = new int[V];
+        int time = 0;
         boolean vis[] = new boolean[V];
-        int time[] = {0};  // ✅ FIXED TIME HANDLING
 
-        for (int i = 0; i < V; i++) {
+        for (int i=0; i<V; i++) {
             if (!vis[i]) {
                 dfs(graph, i, -1, dt, low, vis, time);
             }
         }
     }
-
     public static void main(String[] args) {
         int V = 6;
         @SuppressWarnings("unchecked")
@@ -80,5 +82,6 @@ public class TarjansBridge {
         
         createGraph(graph);
         tarjanBridge(graph, V);
-    }
+
+}
 }
