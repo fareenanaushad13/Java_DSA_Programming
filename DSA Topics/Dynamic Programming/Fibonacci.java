946aa10a5def5a3944b22a9f170264450a5bb077
fareenanaushad13 <114231603+fareenanaushad13@users.noreply.github.com>
2025-03-15 22:41:37 +0530

    Fibonacci

--- DSA Topics/Dynamic Programming/Fibonacci.java
+++ DSA Topics/Dynamic Programming/Fibonacci.java
@@ -1,19 +1,46 @@
 //Time Complexity : O(n)
 public class Fibonacci {
+
+    //recursion //Time Complexity : O(2^n)
     public static int fib(int n, int f[]){
+        if(n==0 || n==1){
+            return n;
+        }
+        return fib(n-1, f) + fib(n-2, f);
+    }
+    
+    //Memoization------ //O(n)
+    public static int fibMemoization(int n, int f[]){
         if(n==0 || n==1){
             return n;
         }
         if(f[n] != 0){ //fib[n] is already calculated
             return f[n];
         }
-        f[n] = fib(n-1, f) + fib(n-2 ,f);
+        f[n] = fibMemoization(n-1, f) + fibMemoization(n-2 ,f);
         return f[n];
     }
+
+
+    //Tabulation------ // O(n)
+    public static int fibTabulation(int n){
+        int dp[] =  new int [n+1];
+        dp[1] = 1;
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n]; //ans
+    }
+
     public static void main(String[] args) {
         int n=5;
         int f[] = new int [n+1]; //0,0,0,0
-        System.out.println(fib(n, f));
+
+        System.out.println(fib(n, f)); //recursion // O(2^n)
+
+        System.out.println(fibMemoization(n, f)); //O(n)
+        
+        System.out.println(fibTabulation(n)); //o(n)
 
     }
 }
