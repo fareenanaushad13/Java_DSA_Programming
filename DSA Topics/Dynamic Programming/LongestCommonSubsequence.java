a3b069f34509863e9ce87d871d6805a19f93bf76
fareenanaushad13 <114231603+fareenanaushad13@users.noreply.github.com>
2025-03-20 01:36:11 +0530

    Longest Common Subsequence

--- DSA Topics/Dynamic Programming/LongestCommonSubsequence.java
+++ DSA Topics/Dynamic Programming/LongestCommonSubsequence.java
@@ -1,52 +1,85 @@
 //Time Complexity -- O(n * m)
 public class LongestCommonSubsequence {
 
-    //Recursion ---> O(2^n)
-    public static int lcs(String str1, String str2, int n , int m){
-        if( n==0 || m ==0){
-            return 0;
-        }
-        if(str1.charAt(n-1) == str2.charAt(m-1)){ //same
-            return lcs(str1, str2, n-1, m-1) + 1;
-        } else{ //different
-            int ans1 = lcs(str1, str2, n-1, m);
-            int ans2 = lcs(str1, str2, n, m-1);
-            return Math.max(ans1, ans2);
-        }
-    }
+    // Recursion ---> O(2^n)
+    // public static int lcs(String str1, String str2, int n , int m){
+    // if( n==0 || m ==0){
+    // return 0;
+    // }
+    // if(str1.charAt(n-1) == str2.charAt(m-1)){ //same
+    // return lcs(str1, str2, n-1, m-1) + 1;
+    // } else{ //different
+    // int ans1 = lcs(str1, str2, n-1, m);
+    // int ans2 = lcs(str1, str2, n, m-1);
+    // return Math.max(ans1, ans2);
+    // }
+    // }
 
+    // Memoization ---> O(n * m)
+    // public static int lcsMemoization(String str1, String str2, int n , int m, int
+    // dp[][]){
+    // if( n==0 || m ==0){
+    // return 0;
+    // }
 
-    //Memoization ---> O(n * m)
-    public static int lcsMemoization(String str1, String str2, int n , int m, int dp[][]){
-        if( n==0 || m ==0){
-            return 0;
-        }
+    // if(dp[n][m] != -1){
+    // return dp[n][m];
+    // }
+    // if(str1.charAt(n-1) == str2.charAt(m-1)){ //same
+    // return dp[n][m] = lcs(str1, str2, n-1, m-1) + 1;
+    // } else{ //different
+    // int ans1 = lcsMemoization(str1, str2, n-1, m,dp);
+    // int ans2 = lcsMemoization(str1, str2, n, m-1,dp);
+    // return dp[n][m] = Math.max(ans1, ans2);
+    // }
+    // }
 
-        if(dp[n][m] != -1){
-            return dp[n][m];
-        }
-        if(str1.charAt(n-1) == str2.charAt(m-1)){ //same
-            return dp[n][m] = lcs(str1, str2, n-1, m-1) + 1;
-        } else{ //different
-            int ans1 = lcsMemoization(str1, str2, n-1, m,dp);
-            int ans2 = lcsMemoization(str1, str2, n, m-1,dp);
-            return dp[n][m] = Math.max(ans1, ans2);
-        }
-    }
-    public static void main(String[] args) {
-        String str1 = "abcdge";
-        String str2 = "abedg"; //lcs --> "abdg", length -->4
-        // System.out.println(lcs(str1, str2, str1.length(), str2.length()));
-
+    // Tabulation ----> O(n * m)
+    public static int lcsTab(String str1, String str2) {
         int n = str1.length();
         int m = str2.length();
-        int dp[][] = new int[n+1][m+1];
-        //initialization
-        for(int i=0;i<n+1;i++){
-            for(int j=0;j<m+1;j++){
-                dp[i][j] = -1;
+
+        int dp[][] = new int[n + 1][m + 1];
+        // initialization
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < m + 1; j++) {
+                if (i == 0 || j == 0) {
+                    dp[j][j] = 0;
+                }
             }
         }
-        System.out.print(lcsMemoization(str1, str2, n, m, dp));
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    int ans1 = dp[i - 1][j];
+                    int ans2 = dp[i][j - 1];
+                    dp[i][j] = Math.max(ans1, ans2);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+
+    public static void main(String[] args) {
+        String str1 = "abcdge";
+        String str2 = "abedg"; // lcs --> "abdg", length -->4
+
+        // System.out.println(lcs(str1, str2, str1.length(), str2.length())); //recursion
+
+        // int n = str1.length(); //for Memoization
+        // int m = str2.length();
+        // int dp[][] = new int[n+1][m+1];
+        // //initialization
+        // for(int i=0;i<n+1;i++){
+        // for(int j=0;j<m+1;j++){
+        // dp[i][j] = -1;
+        // }
+        // }
+        // System.out.print(lcsMemoization(str1, str2, n, m, dp)); //memoization
+
+        System.out.print(lcsTab(str1, str2)); // Tabulation
     }
 }
