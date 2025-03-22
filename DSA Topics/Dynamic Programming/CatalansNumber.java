58ff344320cede9a21f6e3741536ecd0d11b0b68
fareenanaushad13 <114231603+fareenanaushad13@users.noreply.github.com>
2025-03-22 16:43:30 +0530

    Catalan's Number Problem

---
+++ DSA Topics/Dynamic Programming/CatalansNumber.java
@@ -0,0 +1,17 @@
+public class CatalansNumber {
+    //Recursrion
+    public static int catalanRec(int n){
+        if(n == 0 || n ==1){
+            return 1;
+        }
+        int ans = 0; //catalan of n
+        for(int i=0;i<=n-1;i++){
+            ans += catalanRec(i) * catalanRec(n-i-1);
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(catalanRec(n));
+    }
+}
