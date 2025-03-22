cf1aaf8e72d4e8a3680eecfc48b2a526f55cf491
fareenanaushad13 <114231603+fareenanaushad13@users.noreply.github.com>
2025-03-23 00:21:06 +0530

    Matrix Chain Multiplication

--- DSA Topics/Dynamic Programming/MatrixChainMultiplication.java
+++ DSA Topics/Dynamic Programming/MatrixChainMultiplication.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class MatrixChainMultiplication {
     //Recursion ----> O(2^n)
     public static int mcm(int arr[], int i, int j){
@@ -38,16 +36,53 @@ public class MatrixChainMultiplication {
 
         return dp[i][j] = mincost; 
     }
+
+    //Tabulation --- > O(n^3)
+    public static int mcmTabulation(int arr[]){
+        int n = arr.length;
+        int dp[][] = new int[n][n];
+
+        for(int i=0;i<n;i++){
+            dp[i][i] = 0;
+        }
+
+        //bottom up
+        for(int len=2;len<=n-1;len++){
+            for(int i=1;i<=n-len;i++){
+                int j = i+len-1; //col
+                dp[i][j] = Integer.MAX_VALUE;
+                for(int k=i;k<=j-1;k++){
+                    int cost1 = dp[i][k];
+                    int cost2 = dp[k+1][j];
+                    int cost3 = arr[i-1] * arr[k] * arr[j];
+                    dp[i][j] = Math.min(dp[i][j], cost1+cost2+cost3);
+                }
+            }
+        }
+        print(dp);
+        return dp[1][n-1];
+    }
+    public static void print(int dp[][]){
+        for(int i=0;i<dp.length;i++){
+            for(int j=0;j<dp[0].length;j++){
+                System.out.print(dp[i][j]+ " ");
+            }
+            System.err.println();
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,3}; // n=5
-        int n = arr.length;
+        System.out.println(mcmTabulation(arr));
 
         //System.out.println(mcm(arr, 1, n-1));//recursion
 
-        int dp[][] =  new int[n][n];
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i], -1);
-        }
-        System.out.println(mcmMemoization(arr, 1, n-1, dp)); //Memoization
+        // int dp[][] =  new int[n][n];
+        // for(int i=0;i<n;i++){
+        //     Arrays.fill(dp[i], -1);
+        // }
+        // System.out.println(mcmMemoization(arr, 1, n-1, dp)); //Memoization
+
+
     }
 }
