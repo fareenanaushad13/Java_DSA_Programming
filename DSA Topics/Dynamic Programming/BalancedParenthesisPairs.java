9197074bd6681f193803b24e4ecb128b403c56ae
fareenanaushad13 <114231603+fareenanaushad13@users.noreply.github.com>
2025-03-23 02:02:30 +0530

    Combinations of Balanced Parentheses

---
+++ DSA Topics/Dynamic Programming/BalancedParenthesisPairs.java
@@ -0,0 +1,32 @@
+public class BalancedParenthesisPairs {
+    static void printParenthesis(char str[], int pos, int n, int open, int close) {
+        if(close == n)  {
+            for(int i=0;i<str.length;i++){
+                System.out.print(str[i]);
+        
+            }
+            System.out.println();
+            return;
+        }else{
+            if(open > close) {
+                str[pos] = '}';
+                printParenthesis(str, pos+1, n, open, close+1);
+            }
+            if(open < n) {
+                str[pos] = '{';
+                printParenthesis(str, pos+1, n, open+1, close);
+            }
+        }
+    }
+    static void printParenthesis(char str[], int n) {
+        if(n > 0){
+            printParenthesis(str, 0, n, 0, 0);
+            return;
+        }
+    }
+    public static void main(String[] args) {
+        int n =3;
+        char[] str = new char[2 * n];
+        printParenthesis(str, n);
+    }
+}
