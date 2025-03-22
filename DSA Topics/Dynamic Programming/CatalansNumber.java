668cf754787d2a072f56584c00e2550244888c04
fareenanaushad13 <114231603+fareenanaushad13@users.noreply.github.com>
2025-03-22 16:51:20 +0530

    Catalan's Number Problem

--- DSA Topics/Dynamic Programming/CatalansNumber.java
+++ DSA Topics/Dynamic Programming/CatalansNumber.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class CatalansNumber {
     //Recursrion
     public static int catalanRec(int n){
@@ -10,8 +12,31 @@ public class CatalansNumber {
         }
         return ans;
     }
+
+    //Memoization
+    public static int catalanMemoization(int n, int dp[]){
+        if(n == 0 || n ==1){
+            return 1;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        int ans = 0; 
+        for(int i=0;i<n;i++){
+            ans += catalanMemoization(i, dp) * catalanMemoization(n-i-1, dp);
+        }
+        return dp[n]= ans;
+
+    }
+
+
     public static void main(String[] args) {
         int n = 5;
         System.out.println(catalanRec(n));
+
+        int dp[] =  new int[n+1];
+        Arrays.fill(dp, -1);
+        System.out.println(catalanMemoization(4, dp));
     }
 }
