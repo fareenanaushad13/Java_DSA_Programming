086efe32f2b36b2f1d268ced69fa04aa1a854490
fareenanaushad13 <114231603+fareenanaushad13@users.noreply.github.com>
2025-03-23 16:54:45 +0530

    LinkedHashMap

--- DSA Topics/Hashing/LinkedHashMapEx.java
+++ DSA Topics/Hashing/LinkedHashMapEx.java
@@ -1,13 +1,21 @@
 import java.util.*;
 public class LinkedHashMapEx {
     public static void main(String[] args) {
+        //LinkedHashMap
         LinkedHashMap<String, Integer> lhm = new LinkedHashMap<>();
         lhm.put("India", 100);
         lhm.put("Australia", 120);
         lhm.put("China", 150);
         lhm.put("Us", 50);
 
-        System.out.println(lhm); //LinkedHashMap maintains insertion order, whereas HashMap prints elements in a random order
+        //HashMap
+        HashMap<String, Integer> hm = new HashMap<>();
+        hm.put("India", 100);
+        hm.put("Australia", 120);
+        hm.put("China", 150);
+        hm.put("Us", 50);
 
+        System.out.println(lhm); //LinkedHashMap maintains insertion order(India-->Autralia--..), whereas HashMap prints elements in a random order
+        System.out.println(hm);
     }
 }
---
+++ DSA Topics/Hashing/TreeMapEx.java
@@ -0,0 +1,14 @@
+import java.util.TreeMap;
+
+public class TreeMapEx {
+    public static void main(String[] args) {
+        TreeMap<String, Integer> tm = new TreeMap<>();
+        tm.put("India", 100);
+        tm.put("Australia", 120);
+        tm.put("China", 150);
+        tm.put("Us", 50);
+        tm.put("Pakistan", 8);
+
+        System.out.println(tm); //Printed in key sorted order (A → Z)//Autralia-->China...
+    }
+}
