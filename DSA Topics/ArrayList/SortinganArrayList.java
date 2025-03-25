8290b2a5f5360b1e8a95aa62462552388e3be1a1
fareenanaushad13 <114231603+fareenanaushad13@users.noreply.github.com>
2025-03-26 00:31:20 +0530

    Sorting An ArrayList

---
+++ DSA Topics/ArrayList/SortinganArrayList.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SortinganArrayList {
+    public static void main(String[] args) {
+         ArrayList<Integer> list =  new ArrayList<>();
+
+        list.add(1);
+        list.add(5);
+        list.add(7);
+        list.add(3);
+        list.add(6);
+        list.add(1);
+
+        System.out.println(list);
+        Collections.sort(list);
+        System.out.println(list);
+    }
+}
