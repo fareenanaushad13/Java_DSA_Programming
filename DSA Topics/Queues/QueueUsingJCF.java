d60e5fe60d1f9eb67cefb444b1399d25dd26b524
fareenanaushad13 <114231603+fareenanaushad13@users.noreply.github.com>
2025-04-17 20:31:37 +0530

    Queue Using Java Collections Framework

---
+++ DSA Topics/Queues/QueueUsingJCF.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class QueueUsingJCF {
+    public static void main(String[] args) {
+       // QueueB q = new QueueB();
+      // Queue<Integer> q = new LinkedList<>(); //LinkedList
+      Queue<Integer> q = new ArrayDeque<>(); //ArrauDeque
+        q.add(3);
+        q.add(6);
+        q.add(1);
+       
+        while (!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
