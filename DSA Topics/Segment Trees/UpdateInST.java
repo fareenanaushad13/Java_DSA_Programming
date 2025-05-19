094eeee33ff622cda10abb4dc965fcad14a457f7
fareenanaushad13 <114231603+fareenanaushad13@users.noreply.github.com>
2025-05-20 00:01:37 +0530

    Update In Segment Tree

--- DSA Topics/Segment Trees/UpdateInST.java
+++ DSA Topics/Segment Trees/UpdateInST.java
@@ -21,9 +21,9 @@ public class UpdateInST {
     }
 
     public static int getSumUtil(int i, int si, int sj, int qi, int qj) {
-        if (qj <= si || qi >= sj) { 
+        if (qj <= si || qi >= sj) {
             return 0;
-        } else if (si >= qi && sj <= qj) { 
+        } else if (si >= qi && sj <= qj) {
             return tree[i];
 
         } else {
@@ -38,7 +38,6 @@ public class UpdateInST {
 
         int n = arr.length;
         return getSumUtil(0, 0, n - 1, qi, qj);
-
     }
 
     public static void updateUtil(int i, int si, int sj, int idx, int diff) {
@@ -54,7 +53,6 @@ public class UpdateInST {
             updateUtil(2 * i + 2, mid + 1, sj, idx, diff); // right
 
         }
-
     }
 
     public static void update(int arr[], int idx, int newVal) {
@@ -63,9 +61,7 @@ public class UpdateInST {
         arr[idx] = newVal;
 
         updateUtil(0, 0, n - 1, idx, diff);
-
     }
-
     public static void main(String[] args) {
         int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
         int n = arr.length;
